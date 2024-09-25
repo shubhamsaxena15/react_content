b164ba31132e906af74451b0eb6e90028d795686
Shubham Saxena <onlyforimportantpurpose@gmail.com>
2024-09-25 23:58:15 +0530

    2nd commit

---
+++ project1/src/App.js
@@ -0,0 +1,14 @@
+import Firstcomponent from "./Newcomponent";
+
+
+
+function App() {
+  return (
+    <>
+    <Firstcomponent />
+    <h1> Welcome to Java Script with React Library</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ project1/src/Newcomponent.js
@@ -0,0 +1,6 @@
+function Firstcomponent(){
+    return(
+    <h1> we are using component in react project</h1>
+    );
+}
+export default Firstcomponent
